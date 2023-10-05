65199f3acfc02ffc3a389a09b965ccec891a0ba9
KONGRATBAYEV NURZHAN <kongratbayevn@gmail.com>
2023-10-05 15:36:56 +0600

    sketchfab plugin

---
+++ sdkjs-plugins/content/sketchfab/scripts/sketchfab.js
@@ -0,0 +1,8 @@
+(function (window, undefined) {
+    window.Asc.plugin.init = function () {
+
+    };
+    window.Asc.plugin.button = function (id) {
+        this.executeCommand("close", "");
+    };
+})(window, undefined);
