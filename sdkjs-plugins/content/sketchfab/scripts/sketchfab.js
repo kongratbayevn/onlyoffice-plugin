2ec2d4c293f0a4b397c61992f3584406e00f7ef5
KONGRATBAYEV NURZHAN <kongratbayevn@gmail.com>
2023-10-05 15:38:17 +0600

    Revert "sketchfab plugin"
    This reverts commit 65199f3acfc02ffc3a389a09b965ccec891a0ba9.

--- sdkjs-plugins/content/sketchfab/scripts/sketchfab.js
+++
@@ -1,8 +0,0 @@
-(function (window, undefined) {
-    window.Asc.plugin.init = function () {
-
-    };
-    window.Asc.plugin.button = function (id) {
-        this.executeCommand("close", "");
-    };
-})(window, undefined);
